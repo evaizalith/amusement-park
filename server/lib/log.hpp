c4530533fd813c75382b3f0642c9c9af7aaddd58
FoundIzalith <b17kaylaeva@gmail.com>
2023-03-12 23:30:12 -0400

    Add log class to handle logging

---
+++ server/lib/log.hpp
@@ -0,0 +1,26 @@
+//log.hpp
+//This file contains declarations for the Log class.
+//The Log class is used to handle logging debug info and errors. 
+
+#include <iostream>
+#include <vector>
+
+#ifndef GL2_LIB_LOG
+#define GL2_LIB_LOG
+
+class Log {
+public:
+    Log(int log_level) { log_verbosity = log_level; };
+    ~Log();
+
+    //Pass array or vector of strings to both methods
+    //If logging is verbose [log_verbosity = 2], all strings will be printed
+    //If logging is normal [log_verbosity = 1], only index 0 will be printed
+    void log_info(std::vector<std::string>);
+    void log_error(std::vector<std::string>);
+    void log_fatal(std::vector<std::string>);
+private:
+    int log_verbosity;
+};
+
+#endif
