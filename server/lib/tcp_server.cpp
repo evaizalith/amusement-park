9b0222422c8a96589d96566b33cbc02b671fa094
FoundIzalith <b17kaylaeva@gmail.com>
2023-03-15 14:12:25 -0400

    Add start server function

--- server/lib/tcp_server.cpp
+++ server/lib/tcp_server.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <vector>
 #include <thread>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
 #include "tcp_server.hpp"
 #include "log.hpp"
 
@@ -11,13 +14,22 @@ TCPServer::TCPServer(std::string _address, int _port, int _logging) {
 }
 
 TCPServer::~TCPServer() {
-    delete logger;
+    closeServer();
 }
 
 int TCPServer::startServer() {
-    
+    m_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+    if(m_socket < 0) {
+        std::string error = "FAILED TO OPEN SOCKET";
+        logger->log_fatal(error);
+        return 1;
+    }
+
+    return 0;
 }
 
 void TCPServer::closeServer() {
-    delete this; 
+    close(m_socket);
+    delete logger;
 }
