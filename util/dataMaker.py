2ec606ecffed23c188be93f58b56d8f238bd4abf
FoundIzalith <b17kaylaeva@gmail.com>
2023-04-24 18:02:27 -0400

    New connect function

--- util/dataMaker.py
+++ util/dataMaker.py
@@ -1,3 +1,24 @@
 import mysql.connector
-mysql.connector.connect(host='localhost',database='amusement-park',user='root')
+from mysql.connector import Error
 
+def connect():
+    """ Connect to MySQL database """
+    conn = None
+    try:
+        conn = mysql.connector.connect(host='localhost',
+                                       database='amusement-park',
+                                       user='root',
+                                       password='')
+        if conn.is_connected():
+            print('Connected to MySQL database')
+
+    except Error as err:
+        print(err)
+
+    finally:
+        if conn is not None and conn.is_connected():
+            conn.close()
+
+
+if __name__ == 'dataMaker':
+    connect()
